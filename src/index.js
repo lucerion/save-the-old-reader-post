c184f258e975c508ac7e37b7d4dafd70d8b97b6b
lucerion <alexander.skachko@gmail.com>
2020-01-27 13:11:02 +0300

    Save post to file added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
 require('dotenv/config');
 
+const fs = require('fs');
+const os = require('os');
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(''));
+const port = process.env.PORT || 3000;
+const file = process.env.FILE || './posts';
+const lineFormat = process.env.LINE_FORMAT || '%{url} - %{title}';
+
+app.get('/', (req, res) => {
+  fs.appendFile(file, os.EOL, onError);
+  fs.appendFile(file, formattedLine(req.query), onError);
+
+  res.send('OK');
+});
+
+const onError = (error) => error && console.error(error);
+
+const formattedLine = (args, format = lineFormat) => (
+  Object.keys(args).reduce((string, key) => string.replace(`%{${key}}`, args[key]), format)
+);
 
 app.listen(port, () => console.warn(`Listening on port ${port}`));
